23b02d61566de5a59f548e19d91c0438cb2aea09
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-08-22 14:29:22 -0600

    Add 01-const-let

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+const firstName: string = "Viviana";
+let lastName = "Harrizon";
+let diceNumber = 5;
+diceNumber = 3;
+
+console.log(firstName,lastName);
+
+const containsLetterH=  lastName.includes('H');
+console.log({containsLetterH, diceNumber});
---
+++ 01-reforzamiento/src/main.ts
@@ -0,0 +1,9 @@
+import './index.css';
+import './bases/01-const-let';
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div> 
+    <h1>Hola Mundo</h1>
+  </div>
+`
